e0ede8a95e9af0bcafdccddb8fac6e36070342b4
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-16 17:58:58 +0200

    Add toggle button to show/hide message in App component

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 const App = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <p>App</p>
+      <button onClick={() => setShow(!show)}>Show</button>
+      {show && <p style={{ color: "red" }}>Show me baby</p>}
     </>
   );
 };

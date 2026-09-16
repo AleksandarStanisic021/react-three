07cd310a8019c5db7bd374fa1a5b72e36efed83f
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-16 18:55:34 +0200

    Add number list and remove functionality to App component

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 const App = () => {
   const [show, setShow] = useState(false);
+  const [numbers, setNumbers] = useState([1, 2, 3, 4]);
+
+  const remove = (id) => {
+    setNumbers((prevNumbers) => prevNumbers.filter((_, index) => index !== id));
+  };
 
   return (
     <div>
@@ -10,6 +15,12 @@ const App = () => {
         Show
       </button>
       {show && <p style={{ color: "red" }}>Show me baby!</p>}
+      {numbers.map((n, index) => (
+        <div key={index}>
+          <li>{n}</li>
+          <button onClick={() => remove(index)}>remove</button>
+        </div>
+      ))}
     </div>
   );
 };

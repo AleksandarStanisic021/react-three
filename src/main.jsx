38ad43a8f5ebd083206e29110d937e41d7616c70
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-16 17:33:03 +0200

    Add a new function and update render output in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+const fun = () => {
+  return "X";
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <h1>Hi React!</h1>
+    {((e) => "x")()}
   </StrictMode>,
 );

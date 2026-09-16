14e86a091052feb3ee139d34c15a948ccdfba943
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-16 17:49:59 +0200

    Add App component and update main rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
-const fun = () => {
-  return "X";
-};
+import App from "./comps/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <h1>Hi React!</h1>
     {((e) => "x")()}
+    <App />
   </StrictMode>,
 );
